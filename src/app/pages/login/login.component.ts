7e8459a91abde758d5d22021d1914b9e05fdc962
Vinay Ravikumar <vinayravikumar02@gmail.com>
2024-11-02 15:25:45 +0530

    sign in redirection

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 declare var google:any;
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit{
+  private router = inject(Router);
   ngOnInit(): void {
     google.accounts.id.initialize({
       client_id: "313331208405-okj0ubsknrcfpp037j024ml11d8j5fnn.apps.googleusercontent.com",
-      callback:(resp:any)=>{
+      callback:(resp:any)=>{this.handleLoginin(resp)
 
       }
     });
@@ -23,6 +25,24 @@ export class LoginComponent implements OnInit{
       shape:'rectangle',
       width:300
     })
+
+    }
+  private decodeToken(token:string){
+    return JSON.parse(atob(token.split(".")[1]))
+  }
+
+
+    handleLoginin(responce:any){
+      if (responce){
+        const payload = this.decodeToken(responce.credential)
+
+      sessionStorage.setItem("LoggedInuser",JSON.stringify(payload))
+
+      this.router.navigate(["browse"])
+
+
+
+      }
   }
 
 
